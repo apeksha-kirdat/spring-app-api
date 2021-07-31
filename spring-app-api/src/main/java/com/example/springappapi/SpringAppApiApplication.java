2e8f79335b3585cdd8d8375dea57cab3cf5d08be
apeksha <apeksha@DESKTOP-J3P4PL5>
2021-07-31 21:54:08 +0530

    commit

---
+++ spring-app-api/src/main/java/com/example/springappapi/SpringAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApiApplication.class, args);
+	}
+
+}
---
+++ spring-app-api/src/test/java/com/example/springappapi/SpringAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
